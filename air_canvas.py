dc867cd2703aa43ceeaeb21327569f300fee8f72
Flynn Chen <flynn.chen@yale.edu>
2020-08-17 14:33:52 -0400

    tuned color parameters

--- air_canvas.py
+++ air_canvas.py
@@ -23,11 +23,11 @@ learningRate = 0.005
 # HSV required for particular colour 
 cv2.namedWindow("Color detectors")
 cv2.createTrackbar("Upper Hue", "Color detectors", 
-                   153, 180, setValues) 
+                   144, 180, setValues) 
 cv2.createTrackbar("Upper Saturation", "Color detectors", 
                    255, 255, setValues) 
 cv2.createTrackbar("Upper Value", "Color detectors",  
-                   255, 255, setValues) 
+                   224, 255, setValues) 
 cv2.createTrackbar("Lower Hue", "Color detectors", 
                    45, 180, setValues) 
 cv2.createTrackbar("Lower Saturation", "Color detectors",  
